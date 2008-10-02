cab704e50dfe6146380c56724d86a0dd2c6b4830
sivel <sivel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-02 19:39:20 +0000

    Checking in development version to trunk
    git-svn-id: https://plugins.svn.wordpress.org/pagerestrict/trunk@67263 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin.php
+++ inc/admin.php
@@ -4,7 +4,8 @@ Part of WordPress Plugin: Page Restrict
 Plugin URI: http://sivel.net/wordpress/
 */
 
-$pr_version = '1.5';
+//
+$pr_version = '1.5.8.0';
 
 // Check the version in the options table and if less than this version perform update
 function pr_ver_check () {
@@ -15,6 +16,7 @@ function pr_ver_check () {
 		$pr_options['pages'] = explode ( ',' , pr_get_opt ( 'pages' ) );
 		$pr_options['method'] = pr_get_opt ( 'method' );
 		$pr_options['message'] = 'You are required to login to view this page.';
+		$pr_options['loginform'] = true;
 		pr_delete ();
 		add_option ( 'pr_options' , $pr_options , 'Page Restrict Options' );
 	endif;
@@ -24,12 +26,13 @@ function pr_ver_check () {
 function pr_init () {
 	global $pr_version;
 	if ( ! pr_get_opt( 'version' ) ) :
-	        $pr_options = array ();
+		$pr_options = array ();
 		$pr_options['version'] = $pr_version;
-	        $pr_options['pages'] = array ();
+		$pr_options['pages'] = array ();
 		$pr_options['method'] = 'selected';
 		$pr_options['message'] = 'You are required to login to view this page.';
-	        add_option ( 'pr_options', $pr_options, 'Page Restrict Options' ) ;
+		$pr_options['loginform'] = true;
+		add_option ( 'pr_options' , $pr_options ) ;
 	else :
 		pr_ver_check ();
 	endif;
@@ -37,83 +40,193 @@ function pr_init () {
 
 // Delete all options 
 function pr_delete () {
-        delete_option ( 'pr_options' );
+	delete_option ( 'pr_options' );
 }
 
 // Add the options page
 function pr_options_page () {
-	if ( is_admin () ) :
-	        if ( function_exists ( 'add_options_page' ) ) :
-	                add_options_page ( 'Page Restrict' , 'Page Restrict' , 'manage_options' , 'pagerestrict/pagerestrict.php' , 'pr_admin_page' );
-	        endif;
+	if ( function_exists ( 'add_options_page' ) ) :
+		add_options_page ( 'Page Restrict' , 'Page Restrict' , 'manage_options' , 'pagerestrict' , 'pr_admin_page' );
 	endif;
 }
 
 // The options page
 function pr_admin_page () {
-	if ( is_admin () ) :
-		pr_ver_check ();
-		if ( $_POST['submit'] ) :
+	pr_ver_check ();
+	if ( $_POST['action'] == 'update' ) :
+		if ( $_POST['update'] == 'pages' ) :
 			$page_ids = $_POST['page_id'];
-			$pr_options['pages'] = $page_ids;
-			$pr_method = $_POST['method'];
-			if ( count ( $page_ids ) == 0 )
-				$pr_options['method'] = 'none';
-			else
-				$pr_options['method'] = $pr_method;
-			$pr_options['version'] = pr_get_opt ( 'version' );
-			$pr_message = $_POST['message'];
-			$pr_options['message'] = $pr_message;
-			update_option ( 'pr_options' , $pr_options );
 		else :
-			$page_ids = pr_get_opt ( 'pages' );
-			$pr_method = pr_get_opt ( 'method' );
-			$pr_message = pr_get_opt ( 'message' );
+			$page_ids = pr_get_opt ( 'pages' );	
 		endif;
-		if ( $pr_method == 'all' ) :
-			$all_checked = ' checked="checked" ';
-		elseif ( $pr_method == 'none' ) :
-			$none_checked = ' checked="checked" ';
-		else :
-			$selected_checked = ' checked="checked" ';
-		endif;
-		echo '<div class="wrap">' . "\r\n";
-		echo '<h2>Page Restrict Options</h2>' . "\r\n";
-		echo '<form action="' . $_SERVER['REQUEST_URI'] . '" method="post">' . "\r\n";
-		echo '<h3>Enter your restriction message:</h3>' . "\r\n";
-		echo '<input type="text" size="64" name="message" value="' . $pr_message . '" /><br />';
-		echo '<h3>Choose the restriction method:</h3>' . "\r\n";
-		echo '<input type="radio" name="method" value="all"' . $all_checked  . ' />Restrict all pages<br />' . "\r\n";
-		echo '<input type="radio" name="method" value="none"' . $none_checked  . ' />Restrict no pages<br />' . "\r\n";
-		echo '<input type="radio" name="method" value="selected"' . $selected_checked  . ' />Restrict selected pages only<br />' . "\r\n";
-		if ( $pr_method == 'selected' ) :
-			echo '<h3>Select the Pages you wish to restrict to logged in users only:</h3>' . "\r\n";
-			echo '<form action="' . $_SERVER['REQUEST_URI'] . '" method="post">' . "\r\n";
-		        $avail_pages = get_pages ();
-		        $i = count ( $avail_pages );
-			while ( $i > 0 ) :
-				$i--;
-				$pr_page_id = $avail_pages[$i]->ID;	
-				$pr_page_title = $avail_pages[$i]->post_title;
-				if ( $page_ids ) :
-					if ( in_array ( $pr_page_id , $page_ids ) ) :
-						$page_checked = ' checked="checked" ';
-					else :
-						$page_checked = '';
-					endif;
+		$pr_options['pages'] = $page_ids;
+		$pr_method = $_POST['method'];
+		$pr_options['method'] = $pr_method;
+		$pr_options['version'] = pr_get_opt ( 'version' );
+		$pr_message = $_POST['message'];
+		$pr_options['message'] = $pr_message;
+		if ( $_POST['loginform'] == 'true' )
+			$pr_options['loginform'] = true;
+		else
+			$pr_options['loginform'] = false;
+		update_option ( 'pr_options' , $pr_options );
+		echo '<div id="message" class="updated fade"><p><strong>Settings saved.</strong></p></div>';
+	else :
+		$page_ids = pr_get_opt ( 'pages' );
+		$pr_method = pr_get_opt ( 'method' );
+		$pr_message = pr_get_opt ( 'message' );
+	endif;
+?>
+	<div class="wrap">
+		<h2>Page Restrict Options</h2>
+		<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+			<h3>General Options</h3>
+			<p>These options pertain to the gerneral operation of the plugin</p>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">
+						Restriction Message
+					</th>
+					<td>
+						<textarea cols="64" rows="4" name="message"><?php echo $pr_message; ?></textarea>
+						<br />
+						This field can contain HTML.
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						Show Login Form
+					</th>
+					<td>
+						<select name="loginform">
+							<option value="true"<?php selected ( true , pr_get_opt ( 'loginform' ) ); ?>>Yes</option>
+							<option value="false"<?php selected ( false , pr_get_opt ( 'loginform' ) ); ?>>No</option>
+						</select>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						Restriction Method
+					</th>
+					<td>
+						<select name="method">
+							<option value="all"<?php selected ( 'all' , pr_get_opt ( 'method' ) ); ?>>All</option>
+							<option value="none"<?php selected ( 'none' , pr_get_opt ( 'method' ) ); ?>>None</option>
+							<option value="selected"<?php selected ( 'selected' , pr_get_opt ( 'method' ) ); ?>>Selected</option>
+						</select>
+					</td>
+				</tr>
+			</table>
+<?php
+	if ( $pr_method == 'selected' ) :
+?>
+			<h3>Page List</h3>
+			<p>Select the pages that you wish to restrict to logged in users.</p>
+			<input type="hidden" name="update" value="pages" />
+			<table class="form-table">
+<?php
+		$avail_pages = get_pages ();
+		$i = count ( $avail_pages );
+		while ( $i > 0 ) :
+			$i--;
+			$pr_page_id = $avail_pages[$i]->ID;	
+			$pr_page_title = $avail_pages[$i]->post_title;
+			if ( $page_ids ) :
+				if ( in_array ( $pr_page_id , $page_ids ) ) :
+					$page_checked = ' checked="checked" ';
+				else :
+					$page_checked = '';
 				endif;
-				echo '<input type="checkbox" name="page_id[]" value="' . $pr_page_id  . '"' . $page_checked . ' />' . $pr_page_title  . '<br />' . "\r\n";
-			endwhile;
+			endif;
+?>
+				<tr valign="top">
+					<th scope="row">
+						<?php echo $pr_page_title; ?>
+					</th>
+					<td>
+						<input type="checkbox" name="page_id[]" value="<?php echo $pr_page_id; ?>"<?php echo $page_checked; ?> />
+					</td>
+				</tr>
+<?php
+		endwhile;
+?>
+			</table>
+<?php
+	endif;
+?>
+			<br />
+			<p class="submit">
+				<input type="hidden" name="action" value="update" />
+				<input type="submit" name="submit" class="button" value="Save Changes" />
+			</p>
+		</form>
+	</div>
+<?php
+}
+
+/**
+ * The meta box
+ */
+function page_restriction_status_meta_box ( $post ) {
+	$post_ID = $post->ID;
+?>
+	<p>
+		<input name="pr" type="hidden" value="update" />
+		<label for="restriction_status" class="selectit">
+			<input type="checkbox" name="restriction_status" id="restriction_status"<?php if ( in_array ( $post_ID , pr_get_opt ( 'pages' ) ) ) echo ' checked="checked"'; ?>/>
+			Restrict Page
+		</label>
+	</p>
+	<p>These settings apply to this page only. For a full list of restriction statuses see the <a href="options-general.php?page=pagerestrict">global options page</a>.</p>
+<?php
+}
+
+/**
+ * Add meta box to create/edit page pages
+ */
+function pr_meta_box () {
+	add_meta_box ( 'pagerestrictionstatusdiv' , 'Restriction' , 'page_restriction_status_meta_box' , 'page' , 'advanced' , 'high' );
+}
+
+/**
+ * Get custom POST vars on edit/create page pages and update options accordingly
+ */
+function pr_meta_save () {
+	if ( $_POST['pr'] == 'update' ) :
+		$post_ID = $_POST['post_ID'];
+		$restricted_pages = pr_get_opt ( 'pages' );
+		if ( ! is_array ( $restricted_pages ) )
+			$restricted_pages = array ();
+		if ( $_POST['restriction_status'] == 'on' ) :
+			$restricted_pages[] = $post_ID ;
+			$pr_options['pages'] = $restricted_pages;
+		else :
+			$pr_options['pages'] = array_filter ( $restricted_pages , 'pr_array_delete' );
 		endif;
-		echo '<input type="submit" name="submit" class="button" value="Submit" />&nbsp;&nbsp;';
-		echo '<input type="reset" name="reset" class="button" value="Reset" />&nbsp;&nbsp;';
-		echo '<input type="button" name="cancel" value="Cancel" class="button" onclick="javascript:history.go(-1)" />' . "\r\n";
-		echo '</form>' . "\r\n";
-		echo '</div>' . "\r\n";
+		$pr_options['loginform'] = pr_get_opt ( 'loginform' );
+		$pr_options['method'] = pr_get_opt ( 'method' );
+		$pr_options['message'] = pr_get_opt ( 'message' );
+		$pr_options['version'] = pr_get_opt ( 'version' );
+		update_option ( 'pr_options' , $pr_options );
 	endif;
 }
 
-// Add Actions
+/**
+ * Remove item from array
+ */
+function pr_array_delete ( $item ) {
+	return ( $item !== $_POST['post_ID'] );
+}
+
+/**
+ * Activation hook
+ */
+register_activation_hook ( dirname ( dirname ( __FILE__ ) ) . '/pagerestrict.php' , 'pr_init' );
+
+/**
+ * Tell WordPress what to do.  Action hooks.
+ */
+add_action ( 'admin_menu' , 'pr_meta_box' );
+add_action ( 'save_post' , 'pr_meta_save' );
 add_action ( 'admin_menu' , 'pr_options_page' ) ;
-add_action ( 'activate_pagerestrict/pagerestrict.php' , 'pr_init' );
 ?>
--- pagerestrict.php
+++ pagerestrict.php
@@ -5,7 +5,7 @@ Plugin URI: http://sivel.net/wordpress/
 Description: Restrict certain pages to logged in users
 Author: Matt Martz <mdmartz@sivel.net>
 Author URI: http://sivel.net/
-Version: 1.5
+Version: 1.5.8.0
 
 	Copyright (c) 2008 Matt Martz (http://sivel.net)
         Page Restrict is released under the GNU Lesser General Public License (LGPL)
@@ -37,21 +37,24 @@ function pr_no_cache_headers () {
 function pr_page_restrict ( $pr_page_content ) {
 	global $user_ID;
 	get_currentuserinfo ();
-	if ( ! $user_ID ) :
+	if ( ! $user_ID  && is_array ( pr_get_opt ( 'pages' ) ) ) :
 		if ( ( ( is_page ( pr_get_opt ( 'pages' ) ) ) && ( pr_get_opt ( 'method' ) != 'none' ) ) || ( ( is_page () ) && ( pr_get_opt ( 'method' ) == 'all' ) ) ):
 			$pr_page_content = '
-			<p>' . pr_get_opt ( 'message' )  . '</p>
-			<form style="text-align: left;" action="' . get_bloginfo ( 'url' ) . '/wp-login.php" method="post">
-				<p>
-				<label for="log"><input type="text" name="log" id="log" value="' . wp_specialchars ( stripslashes ( $user_login ) , 1 ) . '" size="22" /> User</label><br />
-				<label for="pwd"><input type="password" name="pwd" id="pwd" size="22" /> Password</label><br />
-				<input type="submit" name="submit" value="Log In" class="button" />
-				<label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> Remember me</label><br />
-				</p>
-				<input type="hidden" name="redirect_to" value="' . $_SERVER['REQUEST_URI'] . '" />
-			</form>
-			<p><a href="' . get_bloginfo ( 'url' ) . '/wp-register.php">Register</a>&nbsp;|&nbsp;<a href="' . get_bloginfo ( 'url' ) . '/wp-login.php?action=lostpassword">Lost your password?</a></p>
-			';
+			<p>' . pr_get_opt ( 'message' )  . '</p>';
+			if ( pr_get_opt ( 'loginform' ) == true ) :
+				$pr_page_content .= '
+				<form style="text-align: left;" action="' . get_bloginfo ( 'url' ) . '/wp-login.php" method="post">
+					<p>
+					<label for="log"><input type="text" name="log" id="log" value="' . wp_specialchars ( stripslashes ( $user_login ) , 1 ) . '" size="22" /> User</label><br />
+					<label for="pwd"><input type="password" name="pwd" id="pwd" size="22" /> Password</label><br />
+					<input type="submit" name="submit" value="Log In" class="button" />
+					<label for="rememberme"><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> Remember me</label><br />
+					</p>
+					<input type="hidden" name="redirect_to" value="' . $_SERVER['REQUEST_URI'] . '" />
+				</form>
+				<p><a href="' . get_bloginfo ( 'url' ) . '/wp-register.php">Register</a>&nbsp;|&nbsp;<a href="' . get_bloginfo ( 'url' ) . '/wp-login.php?action=lostpassword">Lost your password?</a></p>
+				';
+			endif;
 		endif;
 	endif;
 	return $pr_page_content;
